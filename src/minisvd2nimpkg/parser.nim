b4877b65a5c0fb25bbac77b3c6ec051a86d809f7
!!Dean <dwhall256@gmail.com>
2024-01-22 15:58:47 -0600

    Add support for derived peripherals
    A derived peripheral is one whose peripheral declaration contains
    a "derivedFrom" attr.  To support this, we cache all peripheral objects
    in a table so a derived peripheral can copy its base.
    
    A derived peripheral should then overwrite the fields copied fields
    of the base, but that is future work.  For now, we just copy the name
    and baseAddress since that is what is in the test file.

--- src/minisvd2nimpkg/parser.nim
+++ src/minisvd2nimpkg/parser.nim
@@ -5,16 +5,30 @@
 ##
 
 import std/paths
+import std/strtabs
 import std/strutils
+import std/tables
 import std/xmlparser
 import std/xmltree
 
 import svdtypes
 
-func parseSvdDevice(deviceNode: XmlNode): SvdDevice
+type PeripheralCache = TableRef[string, SvdPeripheral]
+
+func parseSvdDevice(
+  deviceNode: XmlNode, peripheralCache: var PeripheralCache
+): SvdDevice
 func parseSvdCpu(cpuNode: XmlNode): ref SvdCpu
-func parseSvdPeripherals(peripheralsNode: XmlNode): ref seq[SvdPeripheral]
-func parseSvdPeripheral(peripheralNode: XmlNode): SvdPeripheral
+func parseSvdPeripherals(
+  peripheralsNode: XmlNode, peripheralCache: var PeripheralCache
+): ref seq[SvdPeripheral]
+func parseDerivedSvdPeripheral(
+  peripheralNode: XmlNode, basePeripheral: SvdPeripheral
+): SvdPeripheral
+func parseBaseSvdPeripheral(peripheralNode: XmlNode): SvdPeripheral
+func parseSvdPeripheral(
+  peripheralNode: XmlNode, peripheralCache: var PeripheralCache
+): SvdPeripheral
 func parseSvdInterrupt(interruptNode: XmlNode): ref SvdInterrupt
 func parseSvdAddressBlock(addressBlockNode: XmlNode): ref SvdAddressBlock
 func parseSvdRegisters(registersNode: XmlNode): ref seq[SvdRegister]
@@ -25,11 +39,14 @@ func parseAnyInt(s: string): int
 func parseAnyUInt(s: string): uint
 
 proc parseSvdFile*(fn: Path): SvdDevice =
+  var peripheralCache: PeripheralCache = newTable[string, SvdPeripheral]()
   let xml = loadXml(fn.string)
   assert xml.tag == "device"
-  result = parseSvdDevice(xml)
+  result = parseSvdDevice(xml, peripheralCache)
 
-func parseSvdDevice(deviceNode: XmlNode): SvdDevice =
+func parseSvdDevice(
+    deviceNode: XmlNode, peripheralCache: var PeripheralCache
+): SvdDevice =
   result.name = deviceNode.child("name").innerText
   result.description = deviceNode.child("description").innerText
   result.version = parseFloat(deviceNode.child("version").innerText)
@@ -41,7 +58,7 @@ func parseSvdDevice(deviceNode: XmlNode): SvdDevice =
   let cpuNode = deviceNode.child("cpu")
   result.cpu = parseSvdCpu(cpuNode)
   let peripheralsNode = deviceNode.child("peripherals")
-  result.peripherals = parseSvdPeripherals(peripheralsNode)
+  result.peripherals = parseSvdPeripherals(peripheralsNode, peripheralCache)
 
 func parseSvdCpu(cpuNode: XmlNode): ref SvdCpu =
   if isNil(cpuNode):
@@ -59,14 +76,35 @@ func parseSvdCpu(cpuNode: XmlNode): ref SvdCpu =
   result.nvicPrioBits = parseAnyInt(cpuNode.child("nvicPrioBits").innerText)
   result.vendorSystickConfig = cpuNode.child("vendorSystickConfig").innerText == "true"
 
-func parseSvdPeripherals(peripheralsNode: XmlNode): ref seq[SvdPeripheral] =
+func parseSvdPeripherals(
+    peripheralsNode: XmlNode, peripheralCache: var PeripheralCache
+): ref seq[SvdPeripheral] =
   if isNil(peripheralsNode):
     return nil
   new(result)
   for pnode in peripheralsNode.findAll("peripheral"):
-    result[].add(parseSvdPeripheral(pnode))
+    result[].add(parseSvdPeripheral(pnode, peripheralCache))
 
-func parseSvdPeripheral(peripheralNode: XmlNode): SvdPeripheral =
+func parseSvdPeripheral(
+    peripheralNode: XmlNode, peripheralCache: var PeripheralCache
+): SvdPeripheral =
+  let pattrs = peripheralNode.attrs()
+  if not isNil(pattrs) and "derivedFrom" in pattrs:
+    let basePeripheral = peripheralCache[pattrs["derivedFrom"]]
+    result = parseDerivedSvdPeripheral(peripheralNode, basePeripheral)
+  else:
+    result = parseBaseSvdPeripheral(peripheralNode)
+    peripheralCache[result.name] = result
+
+func parseDerivedSvdPeripheral(
+    peripheralNode: XmlNode, basePeripheral: SvdPeripheral
+): SvdPeripheral =
+  result = basePeripheral # copy
+  # FIXME: parse all children of the derived peripheral
+  result.name = peripheralNode.child("name").innerText
+  result.baseAddress = parseAnyUInt(peripheralNode.child("baseAddress").innerText)
+
+func parseBaseSvdPeripheral(peripheralNode: XmlNode): SvdPeripheral =
   result.name = peripheralNode.child("name").innerText
   result.baseAddress = parseAnyUInt(peripheralNode.child("baseAddress").innerText)
   result.description = peripheralNode.child("description").innerText
@@ -96,7 +134,8 @@ func parseSvdAddressBlock(addressBlockNode: XmlNode): ref SvdAddressBlock =
 func parseSvdRegisters(registersNode: XmlNode): ref seq[SvdRegister] =
   if isNil(registersNode):
     return nil
-  for rnode in registersNode.findAll("peripheral"):
+  new(result)
+  for rnode in registersNode.findAll("register"):
     result[].add(parseSvdRegister(rnode))
 
 func parseSvdRegister(registerNode: XmlNode): SvdRegister =
@@ -110,6 +149,7 @@ func parseSvdRegister(registerNode: XmlNode): SvdRegister =
 func parseSvdFields(fieldsNode: XmlNode): ref seq[SvdRegField] =
   if isNil(fieldsNode):
     return nil
+  new(result)
   for fnode in fieldsNode.findAll("field"):
     result[].add(parseSvdField(fnode))
 
@@ -118,8 +158,14 @@ func parseSvdField(fieldNode: XmlNode): SvdRegField =
   result.description = fieldNode.child("description").innerText
   result.bitOffset = parseInt(fieldNode.child("bitOffset").innerText)
   result.bitWidth = parseInt(fieldNode.child("bitWidth").innerText)
+  let
+    accessText =
+      if isNil(fieldNode.child("access")):
+        "read-only"
+      else:
+        fieldNode.child("access").innerText
   result.access =
-    case fieldNode.child("access").innerText
+    case accessText
     of "read-only":
       SvdRegFieldAccess.readOnly
     of "read-write":
@@ -128,13 +174,15 @@ func parseSvdField(fieldNode: XmlNode): SvdRegField =
       SvdRegFieldAccess.writeOnly
 
 func parseAnyInt(s: string): int =
-  if s.startsWith("0x"):
-    result = parseHexInt(s)
+  let lowercase = s.toLower()
+  if lowercase.startsWith("0x"):
+    result = parseHexInt(lowercase)
   else:
-    result = parseInt(s)
+    result = parseInt(lowercase)
 
 func parseAnyUInt(s: string): uint =
-  if s.startsWith("0x"):
-    result = cast[uint](parseHexInt(s))
+  let lowercase = s.toLower()
+  if lowercase.startsWith("0x"):
+    result = cast[uint](parseHexInt(lowercase))
   else:
     result = parseUInt(s)
