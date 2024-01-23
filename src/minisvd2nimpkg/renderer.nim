ccc7b78e2b4ff92bfa586cda380e55fb26b76d28
!!Dean <dwhall256@gmail.com>
2024-01-22 18:15:50 -0600

    Add more declarations to the renderer
    Also added a "using" block since all proc parameters:types were similar.
    
    Status: The tests parse and render the full STM32 .svd file
    and all Registers and their Fields are declared.
    But the declarations do not yet have complete parameters.

--- src/minisvd2nimpkg/renderer.nim
+++ src/minisvd2nimpkg/renderer.nim
@@ -1,9 +1,6 @@
 ## Renders Nim source to represent a device
 ## according to the given SVD data
 ##
-## Reference:
-##    https://www.keil.com/pack/doc/CMSIS/SVD/html/svd_Format_pg.html
-##
 
 import std/os
 import std/strformat
@@ -12,26 +9,34 @@ import std/strutils
 import svdtypes
 import versions
 
-type Renderer* = ref object
-  device*: SvdDevice
-  outf*: File
 
-proc renderHeader(outf: File, device: SvdDevice)
-proc renderCpu(outf: File, device: SvdDevice)
-proc renderInterrupts(outf: File, device: SvdDevice)
+using
+  outf: File
+  device: SvdDevice
+  peripheral: SvdPeripheral
+  register: SvdRegister
+  field: SvdRegField
 
-proc renderNimFromSvd*(outf: File, device: SvdDevice) =
+proc renderHeader(outf, device)
+proc renderCpu(outf, device)
+proc renderInterrupts(outf, device)
+proc renderPeripherals(outf, device)
+proc renderPeripheral(outf, device, peripheral)
+proc renderRegister(outf, device, peripheral, register)
+proc renderField(outf, device, peripheral, register, field)
+
+proc renderNimFromSvd*(outf, device) =
   renderHeader(outf, device)
   renderCpu(outf, device)
   renderInterrupts(outf, device)
-  # renderPeripherals
-  #   renderRegisters
-  #     renderFields
+  renderPeripherals(outf, device)
 
-proc renderHeader(outf: File, device: SvdDevice) =
+proc renderHeader(outf, device) =
   let filenameParts = getAppFilename().splitFile()
   let toolName = filenameParts.name & filenameParts.ext
-  write(outf, fmt"""
+  write(
+    outf,
+    fmt"""
 # This file is auto-generated.
 # Edits will be lost if the tool is run again.
 #
@@ -44,22 +49,63 @@ import std/volatile
 
 import templates
 
-""")
+"""
+  )
 
-proc renderCpu(outf: File, device: SvdDevice) =
-  write(outf, fmt"""
+proc renderCpu(outf, device) =
+  write(
+    outf,
+    fmt"""
 # CPU details
 const DEVICE* = "{device.name}"
 const MPU_PRESET* = {device.cpu.mpuPresent}
 const FPU_PRESENT* = {device.cpu.fpuPresent}
 const NVIC_PRIO_BITS* = {device.cpu.nvicPrioBits}
 
-""")
+"""
+  )
 
-proc renderInterrupts(outf: File, device: SvdDevice) =
-  write(outf, fmt"""
+proc renderInterrupts(outf, device) =
+  write(
+    outf,
+    fmt"""
 # TODO: fill in this placeholder
 Interrupts* = enum
   discard
 
-""")
+"""
+  )
+
+proc renderPeripherals(outf, device) =
+  if not isNil(device.peripherals):
+    write(outf, "# Peripherals\p")
+    for p in device.peripherals[]:
+      renderPeripheral(outf, device, p)
+
+proc renderPeripheral(outf, device, peripheral) =
+  # We do not output anything specific for a peripheral
+  if not isNil(peripheral.registers):
+    for r in peripheral.registers[]:
+      renderRegister(outf, device, peripheral, r)
+
+proc renderRegister(
+    outf, device, peripheral, register
+) =
+  let regAddress = peripheral.baseAddress + register.addressOffset.uint
+  write(
+    outf,
+    fmt"""
+declareRegister("{peripheral.name}", "{register.name}", 0x{toHex(regAddress, 8)}, "{register.description}")
+"""
+  )
+  if not isNil(register.fields):
+    for f in register.fields[]:
+      renderField(outf, device, peripheral, register, f)
+
+proc renderField( outf, device, peripheral, register, field) =
+  write(
+    outf,
+    fmt"""
+declareField("{peripheral.name}", "{register.name}", "{field.name}", {field.bitOffset}, {field.bitWidth}, {field.access}, "{field.description}")
+"""
+  )
--- tests/test_renderer.nim
+++ tests/test_renderer.nim
@@ -7,22 +7,34 @@ import unittest
 import minisvd2nimpkg/parser
 import minisvd2nimpkg/renderer
 
-let fn = paths.getCurrentDir() / Path("tests") / Path("test.svd")
-let device = parseSvdFile(fn)
+let fn_test = paths.getCurrentDir() / Path("tests") / Path("test.svd")
+let dev_test = parseSvdFile(fn_test)
 
 test "there shall be a procedure to render nim source":
-  check compiles(renderNimFromSvd(stdout, device))
+  check compiles(renderNimFromSvd(stdout, dev_test))
 
 test "DEBUG: generates a file to examine by hand":
   var f = open("tests" / "dwh_test_render.nim", fmWrite)
-  renderNimFromSvd(f, device)
+  renderNimFromSvd(f, dev_test)
 
 test "the render procedure shall output a header comment":
-  let (f,p) = createTempFile("tmp", "test_render.nim")
-  renderNimFromSvd(f, device)
+  let (f, p) = createTempFile("tmp", "test_render.nim")
+  renderNimFromSvd(f, dev_test)
   f.setFilePos(0)
   let line = f.readLine()
   check line[0] == '#'
   check "auto-generated" in line
+  f.close()
+  p.removeFile()
+
+let fn_stm32 = paths.getCurrentDir() / Path("tests") / Path("STM32F446_v1_7.svd")
+let dev_stm32 = parseSvdFile(fn_stm32)
+
+test "the render procedure shall output peripheral registers":
+  let (f, p) = createTempFile("tmp", "stm32_render.nim")
+  renderNimFromSvd(f, dev_stm32)
+  f.setFilePos(0)
+  let fileContents = f.readAll()
+  check "TSTR" in fileContents
   f.close()
   p.removeFile()
