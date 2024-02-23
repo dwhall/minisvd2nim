66932c351568381ca8b4d3700e343c3ec24499cd
!!Dean <dwhall256@gmail.com>
2024-02-23 11:02:28 -0600

    Add test for cli that generates stm32 example

---
+++ tests/test_cli.nim
@@ -0,0 +1,17 @@
+import std/files
+import std/os
+import std/paths
+import unittest
+
+# Check that the project has been built
+let exe = when defined(windows): "minisvd2nim.exe" else: "minisvd2nim"
+let fullExe = paths.getCurrentDir() / Path(exe)
+doAssert fileExists(fullExe), "Build the project before running this test"
+
+test "the CLI should run with no input":
+  let cmd = fullExe.string
+  check 0 == execShellCmd(cmd)
+
+test "the CLI should process the example STM32 .svd file":
+  let cmd = fullExe.string & " tests" / "STM32F446_v1_7.svd > stm32f446.nim"
+  check 0 == execShellCmd(cmd)
