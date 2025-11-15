475b687c4589ef5e566e3ae9fbc7998eacea0085
!!Dean <dwhall256@gmail.com>
2025-11-15 09:01:02 -0600

    Fix unit test runner when path has a space

--- tests/test_cli.nim
+++ tests/test_cli.nim
@@ -1,16 +1,17 @@
-import std/[files, os, paths, unittest]
+import std/[files, os, paths, strutils, unittest]
 
 # Check that the project has been built
 let exe = when defined(windows): "minisvd2nim.exe" else: "minisvd2nim"
 let fullExe = paths.getCurrentDir() / Path(exe)
+let quotedExeStr = "\"" & fullExe.string & "\""
 doAssert fileExists(fullExe), "Build the project before running this test"
 
 test "the CLI should run with no input":
-  let cmd = fullExe.string
+  let cmd = quotedExeStr
   check 0 == execShellCmd(cmd)
 
 test "the CLI should process the example STM32 .svd file":
-  let cmd = fullExe.string & " tests" / "STM32F446_v1_7.svd"
+  let cmd = quotedExeStr & " tests" / "STM32F446_v1_7.svd"
   check 0 == execShellCmd(cmd)
   # remove the directory that was just created by the test
   os.removeDir("stm32f446")
